5d8f6e5f84e4e7b9a7ce77c6bed98bda99cb66ce
rustyBilges <tophermcw@gmail.com>
2024-08-07 06:29:02 +0100

    added save final_mapping on next, and temporary print check

--- streamlit/app.py
+++ streamlit/app.py
@@ -44,6 +44,7 @@ from utilities import run_query, _hide_pages, LocalDatabaseWrapper
 # TODO: add user option to input database backup location and create copy (for later use in modelling)
 
 # TODO FUTURE:
+# TODO: migrate to SQLAlchemy (for pandas sql usage)
 # TODO: Currently assuming all data is numeric. Add select is_string option to attribute_mapping? (other data columns?)
 # TODO: Add prioritisation option? Currently this will be handled automatically.
 # TODO: consider that unused fact tables at UHBW may store important data at other trusts. Document this.
